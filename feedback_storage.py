31da58387d3885bb5b3198a7ae82b1887e304ff9
HenryNVP <henrynguyen.vp@gmail.com>
2024-11-14 18:49:43 -0800

    Delete feedback_storage.py

--- feedback_storage.py
+++
@@ -1,5 +0,0 @@
-def store_feedback(incorrect_fen, incorrect_move, correct_fen, correct_move):
-    # Save the feedback in a file for future retraining
-    with open("corrected_data.txt", "a") as f:
-        f.write(f"Incorrect FEN: {incorrect_fen}, Move: {incorrect_move}\n")
-        f.write(f"Correct FEN: {correct_fen}, Move: {correct_move}\n")
